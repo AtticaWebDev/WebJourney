cd2fc2c1e37a0fe7245ee35b970e1eb773067786
AGBOTON DYLAN <113048599+AtticaWebDev@users.noreply.github.com>
2024-05-13 23:31:40 +0200

    Update config.ts

--- FULLSTACK/FrameFlick/src/firebase/config.ts
+++ FULLSTACK/FrameFlick/src/firebase/config.ts
@@ -1,20 +1,9 @@
-// Import the functions you need from the SDKs you need
+import firebaseConfig from "./firebaseConfig.ts";
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBt90LV1Ci4ZFVnKRUaT8b9X6GAhxuqXXU",
-  authDomain: "frameflick-58cea.firebaseapp.com",
-  projectId: "frameflick-58cea",
-  storageBucket: "frameflick-58cea.appspot.com",
-  messagingSenderId: "548346128104",
-  appId: "1:548346128104:web:cbf2b12e4fecc955800b5d",
-  measurementId: "G-VBDN8QE9CQ",
-};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
